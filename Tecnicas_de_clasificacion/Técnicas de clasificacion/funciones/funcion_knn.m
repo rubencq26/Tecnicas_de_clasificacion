5f04b471fa7efc894c5b986394db58cb07fd82aa
Ruben Conde <144829066+rubencq26@users.noreply.github.com>
2026-05-20 19:01:21 +0200

    Add files via upload

---
+++ Tecnicas_de_clasificacion/Técnicas de clasificacion/funciones/funcion_knn.m
@@ -0,0 +1,9 @@
+function YTest = funcion_knn(XTest, XTrain, YTrain, k)
+    nDatosTrain = size(XTrain,1);
+
+    for i = 1 : nDatosTrain
+        X = XTest(i, :);
+            
+    end
+    
+end
---
+++ Tecnicas_de_clasificacion/Técnicas de clasificacion/funciones/funcion_representa_datos2dim.m
@@ -0,0 +1,30 @@
+function funcion_representa_datos2dim(X, Y)
+    numDatos = size(X, 1);
+    clases = unique(Y);
+    numClases = length(clases);
+    simbolo = ["r*", "b*", "g*", "c*", "m*", "y*", "k*"];
+    
+    
+
+    
+
+    for i = 1 : numClases
+        indices = Y == clases(i);
+
+        plot(X(indices, 1), X(indices, 2), simbolo(i));
+        hold on;
+    end
+        
+        xlabel('Predictor x1');
+        ylabel('Predictor x2');
+
+        texto = strings(1, numClases);
+
+        for i = 1 : numClases
+            texto(i) = "Clase " + clases(i);
+        end
+
+        legend(texto);
+        title('Representación de Datos en 2 Dimensiones');
+        grid on;
+end
